040ae641440eb36669c0031657f62c73744370b6
chamberskaz0301 <76543076+chamberskaz0301@users.noreply.github.com>
2021-05-23 14:36:52 +0900

    へんこう

---
+++ php/html/HelloWorld/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>遅くなった</title>
+</head>
+<body>
+    <h1>これでいいのかな？</h1>
+    <h2>明日から学校です</h2>
+    <h3>おなかすいた</h3>
+    <p>大丈夫ですか？</p>
+</body>
+</html>
